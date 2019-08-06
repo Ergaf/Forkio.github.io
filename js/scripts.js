2bf3ed21137908ad9a24e3b8920d0db2882d600e
Ergaf <Bymbarawko@gmail.com>
2019-08-06 16:44:02 +0300

    saitik?

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+ (function burgerMenu() {
+	 let burgerBtn = document.getElementById('burgerMenuBtn');
+	 burgerBtn.addEventListener('click', function () {
+		 this.classList.toggle('header__burger-menu_opened');
+	 })
+})(); 
+const n = 33;
+const b = 44;
+console.log(44);
